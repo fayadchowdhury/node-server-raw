a96840f59356839c63b4ce6f1270cb1b15057106
fayadchowdhury <fayad.chowdhury@gmail.com>
2022-07-20 01:01:13 +0600

    Set up a basic request and response parsing, with some Buffer code and synchornous code.

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const http = require('http');
-
+const fs = require('fs');
 const PORT = 80;
 
 const server = http.createServer((req, res) => { // this creates an HTTP server taking in a request object and sending back a response object
-  console.log(req.url, req.method);
-  // set the header type
-  res.setHeader('Content-Type', 'text/html');
-  // write the HTMl page line by line (painful!!!)
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>');
-  res.write('NodeJS raw server!')
-  res.write('</title>');
-  res.write('</head>');
-  res.write('<body>');
-  res.write('<h1>');
-  res.write('This is a raw NodeJS server!');
-  res.write('</h1>');
-  res.write('</body>');
-  res.write('</html>');
-  // close the write process
-  res.end();
+  const url = req.url; // get request URL
+  const method = req.method; //get request method
+  
+  if ( url === '/' )
+  {
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>');
+    res.write('NodeJS raw server!')
+    res.write('</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<h1>');
+    res.write('Enter a message');
+    res.write('</h1>');
+    res.write('<form action="/message" method="POST"><input type="text" name="data"><button type="submit">Send!</button></form>'); // create a form that POSTs to /message with a text input named 'data'
+    res.write('</body>');
+    res.write('</html>');
+    return res.end(); // to exit out of the function and not execute any code below 
+  }
+
+  if ( url === '/message' && method === 'POST' )
+  {
+    const body = [];
+    
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }); // attaches a listener to listen for incoming data which arrives in buffered chunks
+    
+    req.on('end', () => {
+      const message = Buffer.concat(body).toString().split('=')[1]; // creates a Buffer object concatenating the message body and then converting it to a string and splits it at delimiter '=' and takes the latter half
+      res.statusCode = 302; // default redirection status code
+      res.setHeader('Location', '/'); // redirects to /
+      fs.writeFileSync('message.txt', message);
+      return res.end();
+    })
+  }
 });
 
 server.listen(PORT); // get the server to listen on PORT
